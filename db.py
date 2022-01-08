50b995d3d951ce1e2dcea04ea3da57dec0d24876
Janith Madarasinghe <janithpm9991@gmail.com>
2022-01-08 07:36:17 +0530

    video and playlist downloader completed

--- db.py
+++ db.py
@@ -1,7 +1,12 @@
-DEFAULT_CONFIGS = [
-    '%USERPROFILE%\Downloads\n',    # Defalut path
-    '720\n',                        # Default resolution
-    'False\n',                      # Auto genarated Subtitles
-    'False\n'                       # Audio only
-]
-CONFIG_FILE = 'config.j'
+# DEFAULT_CONFIGS = [
+#     '%USERPROFILE%\Downloads\n',    # Defalut path
+#     '720\n',                        # Default resolution
+#     'False\n',                      # Auto genarated Subtitles
+#     'False\n'                       # Audio only
+# ]
+# CONFIG_FILE = 'config.j'
+
+import os
+
+path = os.path.join(os.path.dirname(__file__), "mm")
+print(path)
